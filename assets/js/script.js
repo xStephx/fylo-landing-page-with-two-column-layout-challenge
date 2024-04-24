20415bf3c3e181d2ad9c7ec10562a56d3fb61e36
xStephx <bojkovskistefan9@gmail.com>
2024-04-24 23:17:47 +0200

    added subscription section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Hero section email for validator
+// Hero section validator for email
 const emailFormHero = document.getElementById('emailFormHero');
 const emailInputHero = document.getElementById('emailInputHero');
 const emailMessageHero = document.getElementById('emailMessageHero');
@@ -13,6 +13,7 @@ emailFormHero.addEventListener('submit', function (event) {
         emailMessageHero.style.display = 'block';
         emailMessageHero.style.marginTop = '-10px';
         emailMessageHero.style.fontSize = '0.8rem';
+        emailMessageHero.style.color = 'red';
         return;
     }
 
@@ -23,6 +24,7 @@ emailFormHero.addEventListener('submit', function (event) {
         emailMessageHero.style.display = 'block';
         emailMessageHero.style.marginTop = '-10px';
         emailMessageHero.style.fontSize = '0.8rem';
+        emailMessageHero.style.color = 'red';
         return;
     }
 
@@ -33,4 +35,43 @@ emailInputHero.addEventListener('input', function () {
     emailInputHero.classList.remove('is-invalid-hero');
     emailMessageHero.innerText = '';
     emailMessageHero.style.display = 'none';
+});
+
+// Subscription section validator for email
+const emailFormSubscription = document.getElementById('emailFormSubscription');
+const emailInputSubscription = document.getElementById('emailInputSubscription');
+const emailMessageSubscription = document.getElementById('emailMessageSubscription');
+
+emailFormSubscription.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const email = emailInputSubscription.value.trim();
+
+    if (!email) {
+        emailInputSubscription.classList.add('is-invalid');
+        emailMessageSubscription.innerText = 'Please enter your email!';
+        emailMessageSubscription.style.display = 'block';
+        emailMessageSubscription.style.marginTop = '-10px';
+        emailMessageSubscription.style.fontSize = '0.8rem';
+        emailMessageSubscription.style.color = 'white';
+        return;
+    }
+
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+    if (!isValidEmail) {
+        emailInputSubscription.classList.add('is-invalid-subscription');
+        emailMessageSubscription.innerText = 'Please check your email!';
+        emailMessageSubscription.style.display = 'block';
+        emailMessageSubscription.style.marginTop = '-10px';
+        emailMessageSubscription.style.fontSize = '0.8rem';
+        emailMessageSubscription.style.color = 'white';
+        return;
+    }
+
+    emailFormSubscription.submit();
+});
+
+emailInputSubscription.addEventListener('input', function () {
+    emailInputSubscription.classList.remove('is-invalid-subscription');
+    emailMessageSubscription.innerText = '';
+    emailMessageSubscription.style.display = 'none';
 });
